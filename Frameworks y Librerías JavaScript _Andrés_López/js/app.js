37f3718d6b7dd55117178c0b33725f605eaf360f
Santiago Lopez <asantiago0034@gmail.com>
2019-05-07 15:50:24 -0500

    Primera Version

---
+++ Frameworks y Librerías JavaScript _Andrés_López/js/app.js
@@ -0,0 +1,158 @@
+function animarTitulo()
+{
+    var color1 = '#FFFFFF';
+    var color2 = '#DCFF0E';
+    
+
+    $(".main-titulo").delay(500).animate({color: color1},100, function()
+    {
+        $(this).delay(500).animate({color: color2},100, function()
+        {
+          animarTitulo();
+        });
+    });
+}
+
+function agregarImagenes(selector){
+    
+    var colJuego = $(selector);
+    
+    for(var i=0; i<=6; i++){
+        var numero = Math.floor(Math.random() * (5 - 1)) + 1;
+        var imagen = document.createElement("img");
+        $(imagen).attr("src","image/"+numero+".png");
+        $(imagen).addClass("elemento");
+        $(imagen).addClass("img-"+i);
+        $(imagen).appendTo(colJuego);    
+    }
+    
+}
+
+function posicionFichas(){
+    $(".elemento").draggable({
+        revert: true
+    });
+    
+    $(".elemento").droppable({
+        
+        drop: function(event, ui){
+            var elementoDg = $(ui.draggable).attr("src");
+            var elementoDp = $(this).attr("src")
+            
+            $(this).attr("src", elementoDg);
+            $(ui.draggable).attr("src", elementoDp);
+            reglasJuego();
+            
+            rellenarTablero();
+        }
+    })
+}
+
+function darMatriz(i){
+    var fil = $("div[class*='col-"+i+"']").find("img");
+    return fil;
+}
+
+function animar(selector){
+    var puntuacion = $("#score-text").text();
+    $(selector).hide("fast",function(){
+        $(this).remove();
+    })
+              
+
+}
+
+function reglasJuego(){
+    for(var i=1;i<=7;i++){
+        for(var j=0;j<=6;j++){
+            if(i != j && j != i){
+                if($(darMatriz(i)[j]).attr("src")==$(darMatriz(i)[j+1]).attr("src") && $(darMatriz(i)[j]).attr("src")==$(darMatriz(i)[j+2]).attr("src")){
+                    $("#score-text").text(aumentarPuntuacion);
+                    animar(darMatriz(i)[j]);
+                    animar(darMatriz(i)[j+1]);
+                    animar(darMatriz(i)[j+2]);
+                }
+                else if($(darMatriz(i)[j]).attr("src")==$(darMatriz(i)[j-1]).attr("src") && $(darMatriz(i)[j]).attr("src")==$(darMatriz(i)[j-2]).attr("src")){
+                    $("#score-text").text(aumentarPuntuacion);
+                    animar(darMatriz(i)[j]);
+                    animar(darMatriz(i)[j-1]);
+                    animar(darMatriz(i)[j-2]);
+                }
+                else if($(darMatriz(i)[j]).attr("src")==$(darMatriz(i+1)[j]).attr("src") && $(darMatriz(i)[j]).attr("src")==$(darMatriz(i+2)[j]).attr("src")){
+                    $("#score-text").text(aumentarPuntuacion);
+                    animar(darMatriz(i)[j]);
+                    animar(darMatriz(i+1)[j]);
+                    animar(darMatriz(i+2)[j]);
+                }
+                else if($(darMatriz(i)[j]).attr("src")==$(darMatriz(i-1)[j]).attr("src") && $(darMatriz(i)[j]).attr("src")==$(darMatriz(i-2)[j]).attr("src")){
+                    $("#score-text").text(aumentarPuntuacion);
+                    animar(darMatriz(i)[j]);
+                    animar(darMatriz(i-1)[j]);
+                    animar(darMatriz(i-2)[j]);
+                }   
+            }                
+        }
+    }
+}
+
+function rellenarTablero(){
+    var constante = 7;
+    for(var i=1; i<=7; i++){
+        var imagenesCol = $("div[class*='col-"+i+"']").find("img");
+        var selector = $("div[class*='col-"+i+"']");
+        var imgRestantes = imagenesCol.length;
+        var imgFaltantes = constante-imgRestantes;
+        
+        if(imgRestantes != 7){
+            for(var j=1; j<=imgFaltantes; j++){
+                var numero = Math.floor(Math.random() * (5 - 1)) + 1;
+                var imagen = document.createElement("img");
+                $(imagen).attr("src","image/"+numero+".png");
+                $(imagen).addClass("elemento");
+                $(imagen).appendTo(selector);
+            }
+        }
+    }
+    
+}
+
+function aumentarPuntuacion(){
+    var punt = parseInt($("#score-text").text());
+    return punt+5;
+}
+
+$(function(){
+    animarTitulo();
+    
+    
+    $(".btn-reinicio").on("click",function(){
+        for(var i=1; i<8; i++){
+            agregarImagenes($(".col-"+i));    
+        }
+        $(this).attr("disabled","true");
+        posicionFichas();
+
+        //darMatriz();
+        //reglasJuego();
+        /* if($("div[class*='col-1']").find("img").length<7)
+        {
+             var numero = Math.floor(Math.random() * (5 - 1)) + 1;
+            var imagen = document.createElement("img");
+            $(imagen).attr("src","image/"+numero+".png");
+            $(imagen).addClass("elemento");
+            
+            $(imagen).appendTo($("div[class*='col-1']"));
+        }*/
+        $(".elemento").hover(function(){
+        rellenarTablero();
+        posicionFichas();
+    })
+        
+    });
+   
+    
+    
+    
+    
+    
+})
